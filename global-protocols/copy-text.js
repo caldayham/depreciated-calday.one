f6b612e8504fb7999b55d62b36e10a88be069b34
caldayham <caldayham@gmail.com>
2022-12-03 18:53:22 -0800

    new SPA single page application dev branch

--- global-protocols/copy-text.js
+++
@@ -1,7 +0,0 @@
-function copyToClip(msg) {
-    navigator.clipboard.writeText(msg).then(() => {
-      alert(`${msg} successfully copied to clipboard`);
-    }, () => {
-      alert(`${msg} failed to copy to clipboard`)
-    });
-}
