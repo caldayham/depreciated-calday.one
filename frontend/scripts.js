26eac2c144aca9ea4cfe8f9f0c9fbb84279c0d9b
caldayham <caldayham@gmail.com>
2022-12-03 19:07:03 -0800

    backend node express server setup

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -18,7 +18,13 @@ checkbox.addEventListener('change', () => {
     localStorage.setItem('theme', targetTheme);
 });
 
-
+function copyToClip(msg) {
+    navigator.clipboard.writeText(msg).then(() => {
+      alert(`${msg} successfully copied to clipboard`);
+    }, () => {
+      alert(`${msg} failed to copy to clipboard`)
+    });
+}
 
 
 
